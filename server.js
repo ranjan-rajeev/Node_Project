379d32f242d8fa8b107abf8c4ca8bc5908a6087e
RAJEEV RANJAN <rajeev15069@gmail.com>
2018-05-24 01:14:36 +0530

    changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoDb  = require('mongodb').MongoClient
+const MongoClient  = require('mongodb').MongoClient
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json());
 
 app.listen(1000,function(){
 	console.log('Server Running ON Port 1000 ');
@@ -24,12 +24,66 @@ app.post('/quotes',function(req,res){
 })
 
 
-var db
-mongoDb.connect('mongodb://rajeev:rajeev@ds131800.mlab.com:31800/firstdatabase', { useNewUrlParser: true },function(err,client){
+
+
+app.post('/db',authMiddleware,requestMiddleWare,function(req,res,next){
+
+var data=req.body;
+
+
+if(data.hasOwnProperty('productId')){
+	if(data['productId']=='1')
+		data['ProductName']='Bike Insurance'
+	else if(data['productId']=='10')
+		data['ProductName']='Car Insurance'
+}
+
+res.send(data);
+/*MongoClient.connect('mongodb://rajeev:rajeev@ds131800.mlab.com:31800/firstdatabase', { useNewUrlParser: true },function(err,client){
 	if(err)
 		return console.log(err);
-	db = client.db('firstdatabase')
-	app.listen(3000,function(){
-		console.log('Db listening on port 3000')
-	})
-})
+	var db = client.db('firstdatabase');
+	db.collection('quotes').find({"name":"rajeev ranjan"}).toArray(function(err,result){
+		if (err) 
+			{
+				return console.log(err);
+			}
+			else{
+				res.send(result);
+		console.log(result);
+	}
+	})*/
+});
+function authMiddleware(req,res,next)
+{
+	var data=req.headers;
+	if(data['key']=='123456789'){
+		return next();
+	}
+	else{
+		return res.status(401).json({Message:'Not Authorized'});
+	}
+}
+
+function requestMiddleWare(req,res,next){
+
+		var data = req.body;
+		if(data.hasOwnProperty('productId')){
+			if(data['productId']=='10'  || data['productId']=='1')
+				return next();
+			else
+				return res.status(400).json({Message:'Product Id Should be 10'});
+
+		}else{
+			return res.status(400).json({Message:'Please send ProductID'});
+		}	
+}
+
+
+
+
+
+
+
+
+
